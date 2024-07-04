76426ff0dc466ad1cb0469ea71cd93909a5979e0
devb0x <moniezgeoffrey59@gmail.com>
2024-07-04 23:33:35 +0200

    add navbar animation and css mobile

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core'
-import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router"
+import {NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router"
 import { CommonModule } from "@angular/common"
 
-import { Subscription } from "rxjs"
+import {filter, Subscription} from "rxjs"
 
 import { AuthService } from "../../auth/auth.service"
 
@@ -19,10 +19,15 @@ import { AuthService } from "../../auth/auth.service"
 	styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-	userIsAuth = false
+	userIsAuth:boolean = false
 	private authListenerSubs!: Subscription
+	private routerEventsSub!: Subscription
+	mobileMenuOpen: boolean = false
 
-	constructor(private authService: AuthService) {}
+	constructor(
+		private authService: AuthService,
+		private router: Router
+	) {}
 
 	ngOnInit() {
 		this.userIsAuth = this.authService.getIsAuth()
@@ -31,10 +36,71 @@ export class NavbarComponent {
 			.subscribe(isUserAuth => {
 				this.userIsAuth = isUserAuth
 			})
+		// Listen to router events
+		// this.router.events.subscribe(event => {
+		// 	if (event instanceof NavigationEnd) {
+		// 		// Handle navigation end event, update component state as needed
+		// 		console.log('call from ngOnInit lifecycle')
+		// 		this.userIsAuth = this.authService.getIsAuth();
+		// 		// console.log(this.mobileMenuOpen)
+		// 		this.mobileMenuOpen = false
+		// 		const mobileNav = document.querySelector('.mobile-navbar') as HTMLElement
+		// 		if (mobileNav) {
+		// 			console.warn('called #2')
+		// 			console.log(mobileNav)
+		// 			mobileNav.classList.toggle('display')
+		// 			mobileNav.classList.add('hidden')
+		// 			mobileNav.style.display = 'none'
+		//
+		// 			// this.mobileMenuOpen = false
+		// 		}
+		// 		// console.log(this.mobileMenuOpen)
+		// 	}
+		// });
+
 	}
+
+	// ngAfterViewInit() {
+	// 	const mobileNav = document.querySelector('.mobile-navbar') as HTMLElement
+	// 	if (mobileNav) {
+	// 		console.warn('called #2')
+	// 		console.log(mobileNav)
+	// 		mobileNav.classList.remove('display')
+	// 		mobileNav.style.display = 'flex'
+	// 	}
+	// }
 
 	ngOnDestroy() {
 		this.authListenerSubs.unsubscribe()
+		this.routerEventsSub.unsubscribe()
+	}
+
+	toggleMobileNav() {
+		console.log('toggleMobileNav() clicked ')
+		this.mobileMenuOpen = !this.mobileMenuOpen;
+		if (!this.mobileMenuOpen) {
+			this.hiddeMobileNav()
+		}
+		setTimeout(() => {
+			const ul = document.querySelector('.mobile-navbar-list') as HTMLElement
+			ul.classList.toggle('translate')
+		}, 50)
+	}
+
+	hiddeMobileNav() {
+		console.log('hiddeMobileNav() called')
+		this.mobileMenuOpen = false
+		const mobileNav = document.querySelector('.mobile-navbar') as HTMLElement
+		if (mobileNav) {
+			mobileNav.classList.remove('display')
+			mobileNav.classList.add('hidden')
+			// mobileNav.style.display = 'none'
+		}
+	}
+
+	closeMobileNav() {
+		console.log('closeCustom called')
+		this.toggleMobileNav()
 	}
 
 }
